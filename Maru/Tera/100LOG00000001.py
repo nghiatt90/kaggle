cad7f73b3688b7c3243e2984df70dd251e2edbe9
Chie <t.mai0025@gmail.com>
2017-11-22 17:55:19 +0900

    develop report

---
+++ Maru/Tera/100LOG00000001.py
@@ -0,0 +1,189 @@
+# coding: utf-8
+
+# In[1]:
+
+
+import re
+import pandas as pd
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+labels = ['WT', 'SF', 'BT', 'PR', 'PF', 'M1', 'M2', 'M3', 'M4', 'M5', 'IO', 'IS', 'IC', 'CO', 'CC', 'CP', 'CS', 'F1',
+          'F2', 'FD', 'MD', 'TP']
+date_str = ''
+
+
+# In[3]:
+
+
+def get_df(col, orig):
+    ret = pd.DataFrame({'time': orig['time'], col: orig[col]})
+    ret = ret.set_index('time')
+    return ret.dropna()
+
+
+# In[4]:
+
+
+def process_line(m):
+    time = datetime.datetime.strptime(date_str + ' ' + m.group(1)[:-4], '%Y%m%d %H:%M:%S')
+    code = m.group(2)
+    value = m.group(3)
+    return [time, code, float(value)]
+
+
+# In[5]:
+
+
+def process_line2(m):
+    time = datetime.datetime.strptime(date_str + ' ' + m.group(1)[:-4], '%Y%m%d %H:%M:%S')
+    code = m.group(2)
+    params = m.group(3).split(',')
+    sensor = params[0]
+    values = [int(x[x.find(':') + 1:]) for x in params[1:]]
+    values = {labels[i]: values[i] for i in range(len(labels))}
+    return time, code, sensor, values
+
+
+# In[6]:
+
+
+def process_file(input_file):
+    pattern_str1 = '([0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.[0-9]*).*\"(SO#70[0-9][0-9])\[([0-9]*)(g|h|ms|s|sec|A).*].*"'
+    pattern1 = re.compile(pattern_str1)
+    pattern3 = "[0-9]{8}"
+    pattern3_str3 = re.compile(pattern3)
+    m = pattern3_str3.search(input_file)
+    global date_str
+    if m:
+        date_str = m.group(0)
+
+    all_data1 = []
+    with open(input_file) as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            m1 = pattern1.match(line)
+            if m1 is not None:
+                all_data1.append(process_line(m1))
+    df = pd.DataFrame(all_data1, columns=['time', 'code', 'value'])
+    new_df = pd.DataFrame({'time': df['time'], 'Infeed shutter open': df['value'].where('SO#7002' == df['code']),
+                           'Infeed shutter close': df['value'].where('SO#7003' == df['code']),
+                           'Infeed shutter close. delay mode': df['value'].where('SO#7004' == df['code']),
+                           'Compactor shutter open': df['value'].where('SO#7005' == df['code']),
+                           'Compactor shutter close': df['value'].where('SO#7006' == df['code']),
+                           'Compactor move delay': df['value'].where('SO#7007' == df['code']),
+                           'Bottle weight': df['value'].where('SO#7008' == df['code']),
+                           'Cleaning time.': df['value'].where('SO#7009' == df['code']),
+                           'Inverter operating time': df['value'].where('SO#7012' == df['code']),
+                           'Inverter current peak/Reset/60Hz': df['value'].where('SO#7013' == df['code']),
+                           'Inverter current peak/Reset/75Hz': df['value'].where('SO#7014' == df['code']),
+                           'Inverter current peak/60Hz': df['value'].where('SO#7015' == df['code']),
+                           'Inverter current peak/75Hz': df['value'].where('SO#7016' == df['code']),
+                           'Weight peak/Reset': df['value'].where('SO#7017' == df['code']),
+                           'Weight peak/Compaction': df['value'].where('SO#7018' == df['code'])})
+    return new_df
+
+
+# In[7]:
+
+
+def process_file2(input_file):
+    pattern_str2 = '([0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.[0-9]*).*(SO#7001)\[(.*)].*'
+    pattern2 = re.compile(pattern_str2)
+    pattern3 = "[0-9]{8}"
+    pattern3_str3 = re.compile(pattern3)
+    m = pattern3_str3.search(input_file)
+    global date_str
+    if m:
+        date_str = m.group(0)
+    all_data2 = []
+    with open(input_file) as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            m2 = pattern2.match(line)
+            if m2 is not None:
+                all_data2.append(process_line2(m2))
+    df2 = pd.DataFrame(all_data2, columns=['time', 'code', 'sensor', 'values'])
+    new_df2 = pd.DataFrame({'time': df2['time'], 'code': df2['code'], 'sensor': df2['sensor']})
+    vals = pd.DataFrame.from_records(df2['values'].values)
+    new_df2 = new_df2.join(vals)
+    return new_df2
+
+
+# In[11]:
+
+
+def plot_columns(df, columns, ax=None):
+    for col in columns:
+        x = get_df(col, df)
+        if x[col].isnull().all():
+            continue
+        if ax is None:
+            ax = x.plot()
+            ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        else:
+            x.plot(ax=ax)
+            ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+
+# In[12]:
+
+
+total_df = pd.DataFrame()
+total_df2 = pd.DataFrame()
+for f in ('main_20171102.log', 'main_20171103.log', 'main_20171104.log', 'main_20171105.log',
+          'main_20171106.log', 'main_20171107.log', 'main_20171108.log', 'main_20171109.log',
+          'main_20171110.log', 'main_20171111.log', 'main_20171112.log', 'main_20171113.log',
+          'main_20171114.log', 'main_20171115.log', 'main_20171116.log', 'main_20171117.log',
+          'main_20171118.log', 'main_20171119.log', 'main_20171120.log', 'main_20171121.log',
+          'main_20171122.log'):
+    total_df = total_df.append(process_file(f))
+    total_df2 = total_df2.append(process_file2(f))
+
+print total_df2
+# In[13]:
+#total_df2['WT'].to_frame().boxplot()
+
+# new_total_df2 = pd.DataFrame()
+# new_total_df2.append(total_df2[0:3]).append(total_df2[7:20])
+# new_total_df2.boxplot()
+
+#total_df2.iloc[:, 0:20].boxplot()
+#total_df2['BT'].to_frame().boxplot()
+#plt.show()
+
+# print total_df.describe()
+#print total_df2['WT'].describe()
+
+
+# total_df['Weight peak/Compaction'].to_frame().boxplot()
+# print total_df['Weight peak/Compaction'].describe()
+# plt.show()
+
+# In[14]:
+
+
+# #plot_columns(total_df, ['Infeed shutter open', 'Infeed shutter close', 'Infeed shutter close. delay mode',
+#                         'Compactor shutter open', 'Compactor shutter close', 'Compactor move delay'])
+# #plot_columns(total_df,
+#              ['Bottle weight', 'Cleaning time.', 'Inverter operating time', 'Inverter current peak/Reset/60Hz',
+#               'Inverter current peak/Reset/75Hz', 'Inverter current peak/60Hz', 'Inverter current peak/75Hz',
+#               'Weight peak/Reset', 'Weight peak/Compaction'])
+# #plot_columns(total_df2, labels)
+
+# In[15]:
+
+plt.hist(total_df2['sensor'])
+plt.show()
+# total_df2.boxplot()
+# plt.show()
+
